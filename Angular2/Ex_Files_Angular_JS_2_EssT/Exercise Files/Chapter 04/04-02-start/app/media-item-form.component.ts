759384ce312c4d06914414d12724934483826d14
bithu30 <bijith.komalan@gmail.com>
2016-08-04 12:41:10 +0100

    20160804 LT checkin

--- Angular2/Ex_Files_Angular_JS_2_EssT/Exercise Files/Chapter 04/04-02-start/app/media-item-form.component.ts
+++ Angular2/Ex_Files_Angular_JS_2_EssT/Exercise Files/Chapter 04/04-02-start/app/media-item-form.component.ts
@@ -6,4 +6,8 @@ import {Component} from 'angular2/core';
     styleUrls: ['app/media-item-form.component.css']
 })
 export class MediaItemFormComponent {
+
+    onSubmit(mediaItem) {
+        console.log(mediaItem);
+    }
 }
