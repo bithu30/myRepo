cce9b02080950e3d07ca7002c64f522962bcd7d6
bith30 <bijith.komalan@gmail.com>
2016-08-27 20:26:22 +0100

    PIPS VM checkin 20160827

---
+++ python_snippets/PIPS/.idea/pips_gw_test.py
@@ -0,0 +1,12 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+with open ('/home/bithu/Desktop/PIPS/sea_level_rise.xml','r') as cont:
+    cont_xml = cont.read()
+
+
+#cont_xml=''
+headers = {'Content-Type': 'application/xml'}
+response = requests.post('http://localhost:8080/geonetwork/srv/eng/csw-publication', data=cont_xml, headers=headers,auth=HTTPBasicAuth('admin','admin'))
+print(response.text)
+
