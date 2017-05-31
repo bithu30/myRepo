1a7e0e41955610370ad5c245047977a303ce1e7c
Bijith Komalan <bijith.komalan@gmail.com>
2017-05-31 07:30:51 +0100

    Laptop Checkin 20170530

--- Ex_Files_ReactJS_Basics/Ch06/06_01/finish/bulletin-board/src/App.js
+++ Ex_Files_ReactJS_Basics/Ch06/06_01/finish/bulletin-board/src/App.js
@@ -1,21 +1,79 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+var Board = React.createClass({
+    propTypes: {
+        count: function(props, propName) {
+            if(typeof props[propName] !== "number") {
+                return new Error("the count must be a number")
+            }
 
-export default App;
+            if(props[propName] > 100) {
+                return new Error('Creating ' + props[propName] + ' notes is ridiculous')
+            }
+        }
+    },
+    getInitialState() {
+        return {
+            notes: []
+        }
+    },
+    componentWillMount() {
+        if (this.props.count) {
+            var url = `http://baconipsum.com/api/?type=all-meat&sentences=${this.props.count}`
+            fetch(url)
+                  .then(results => results.json())
+                  .then(array => array[0])
+                  .then(text => text.split('. '))
+                  .then(array => array.forEach(
+                        sentence => this.add(sentence)))
+                  .catch(function(err) {
+                    console.log("Didn't connect to the API", err)
+                  })
+        }
+    },
+    nextId() {
+        this.uniqueId = this.uniqueId || 0
+        return this.uniqueId++
+    },
+    add(text) {
+        var notes = [
+            ...this.state.notes,
+            {
+                id: this.nextId(),
+                note: text
+            }
+        ]
+        this.setState({notes})
+    },
+    update(newText, id) {
+        var notes = this.state.notes.map(
+            note => (note.id !== id) ?
+               note :
+                {
+                    ...note,
+                    note: newText
+                }
+            )
+        this.setState({notes})
+    },
+    remove(id) {
+        var notes = this.state.notes.filter(note => note.id !== id)
+        this.setState({notes})
+    },
+    eachNote(note) {
+        return (<Note key={note.id}
+                      id={note.id}
+                      onChange={this.update}
+                      onRemove={this.remove}>
+                  {note.note}
+                </Note>)
+    },
+    render() {
+        return (<div className='board'>
+                   {this.state.notes.map(this.eachNote)}
+                   <button onClick={() => this.add('New Note')}>+</button>
+                </div>)
+    }
+})
+export default Board;
--- python_snippets/intPython/String_ops.py
+++ python_snippets/intPython/String_ops.py
@@ -1,3 +1,5 @@
+import  os
+
 names = ['Declan','Bijith','Mohammed','Slaw']
 print (','.join(names))
 #hello= map(lambda x: 'Hello '+x,names)
@@ -8,3 +10,13 @@ how_many=12
 
 print("{} brought {} apples today".format(who,how_many))
 
+loc="c\\myRepo"
+fname = "test.py"
+
+
+print(os.path.join(loc,fname))
+
+age=40
+name="Bijith"
+print('{} is {} years of age'.format(name,age))
+print('{0} is {1} years of age'.format(name,age))
