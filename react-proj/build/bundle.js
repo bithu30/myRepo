53561f2e07e40c6dcebfd01563618e830ac53153
bithu30 <bijith.komalan@gmail.com>
2017-06-03 16:08:54 +0100

    asus checkin 20170603

--- react-proj/build/bundle.js
+++ react-proj/build/bundle.js
@@ -9546,7 +9546,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 _reactDom2.default.render(_react2.default.createElement(
    'h1',
    null,
-   'Hello, world!'
+   'Hello, world!!!'
 ), document.getElementById('react-container'));
 
 /***/ }),
---
+++ react-proj/functional.js
@@ -0,0 +1,5 @@
+var num = [1,2,3]
+
+for ( var i = 0 ; i < num.length;i++) {
+    console.log(num[i])
+}
