447506426851bf46be54d78053eba49c73e83795
bithu30 <bijith.komalan@gmail.com>
2017-06-03 23:22:35 +0100

    asus checkin 20170603 1

--- react-proj/functional.js
+++ react-proj/functional.js
@@ -1,5 +1,13 @@
 var num = [1,2,3]
 
-for ( var i = 0 ; i < num.length;i++) {
+/*for ( var i = 0 ; i < num.length;i++) {
     console.log(num[i])
-}
+}*/
+
+num.forEach( function(number){ console.log(number)})
+
+num.forEach ( function(number, i) {
+    num[i] = number * 2;
+    console.log(num[i]);
+
+})
