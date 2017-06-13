078d732dcf0c753b77e5c3eae5c1993b1df2e756
Bijith Komalan <bijith.komalan@gmail.com>
2017-06-13 20:08:33 +0100

    Laptop Checkin 20170613 Sesson2

--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_pre_proc.R
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_pre_proc.R
@@ -1,5 +1,8 @@
-scdf = read.csv('Data.csv')
+df = read.csv('Data.csv')
 #Handling Missing Data
 df$Age = ifelse(is.na(df$Age), ave(df$Age, FUN = function(x) mean(x,na.rm = TRUE) ),df$Age)
 df$Salary = ifelse(is.na(df$Salary), ave(df$Salary, FUN = function(x) mean(x,na.rm = TRUE) ),df$Salary)
-df
+
+#encoding Categorical data
+df$Country = factor(df$Country,levels = c('France','Spain','Germany'),labels=c(1,2,3))
+df$Purchased = factor(df$Purchased,levels = c('No','Yes'),labels=c(0,1))
--- Machine Learning A-Z/Part 1 - Data Preprocessing/data_pre_proc.py
+++ Machine Learning A-Z/Part 1 - Data Preprocessing/data_pre_proc.py
@@ -6,7 +6,7 @@ import pandas as pd
 df = pd.read_csv('Data.csv')
 X = df.iloc[:,:-1].values
 y = df.iloc[:,3].values
-print(X)
+#print(X)
 
 #Taking Care of missing data, done using Imputer class
 #from sklearn.preprocessing
@@ -14,11 +14,20 @@ from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN',strategy="mean",axis=0)
 imputer = imputer.fit(X[:,1:3]) #3 because the upper-bound is always excluded in python
 X[:,1:3] = imputer.transform(X[:,1:3])
-print(X)
+#print(X)
 
 #Taking care of categorical data
 
-from sklearn.preprocessing import  LabelEncoder
+from sklearn.preprocessing import  LabelEncoder, OneHotEncoder
 labelEncoder_X = LabelEncoder()
 X[:,0] = labelEncoder_X.fit_transform(X[:,0])
-print(X)
+#print(X)
+#Creating Dummy variables
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X=oneHotEncoder.fit_transform(X).toarray()
+#print(X)
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+print(y)
