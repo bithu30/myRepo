bbebc0e381b09919385d3ff9269d8dc46e29604b
Bijith Komalan <bijith.komalan@gmail.com>
2017-06-16 18:07:50 +0100

    Laptop checkin 20170616

--- R/Analytic Edge/Code/HW1.R
+++ R/Analytic Edge/Code/HW1.R
@@ -20,3 +20,25 @@ hist(mvt$Year, breaks=100)
 boxplot(mvt$Date~mvt$Arrest)
 mvt <- read.csv('mvtWeek1.csv')
 mvt_new<-read.csv('mvt_new.csv')
+
+#for section4 Q1-5
+
+sort(table(mvt$LocationDescription),decreasing = TRUE)
+
+Top5 = subset(mvt_new,mvt_new$LocationDescription == 'STREET' | mvt_new$LocationDescription == 'ALLEY'| 
+                mvt_new$LocationDescription == 'PARKING LOT/GARAGE(NON.RESID.)' |  mvt_new$LocationDescription == 'DRIVEWAY - RESIDENTIAL'|
+                mvt_new$LocationDescription == 'GAS STATION')
+
+str(Top5)
+
+Top5$LocationDescription = factor(Top5$LocationDescription)
+
+table(Top5$LocationDescription,Top5$Arrest)
+
+gas_station = subset(Top5,Top5$LocationDescription=='GAS STATION')
+
+table(gas_station$Weekday)
+
+driveway = subset(Top5,Top5$LocationDescription=='DRIVEWAY - RESIDENTIAL')
+
+table(driveway$Weekday)
