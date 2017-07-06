5422b5b54fd41386590cb79d4806cc3be98a57d0
Bijith Komalan <bijith.komalan@gmail.com>
2017-07-06 15:16:33 +0100

    Laptop Checkin 20170706

--- R/Analytic Edge/Code/HW1_4w.R
+++ R/Analytic Edge/Code/HW1_4w.R
@@ -15,7 +15,15 @@ table(hawvote)/length(hawvote)
 selfvote <- gerber$self[gerber$voting==1]
 table(selfvote)/length(selfvote)
 
-glm_grber <- glm(voting ~ civicduty+hawthorne+self+neighbors, data = gerber, family = binomial)
+
+glm_grber <- glm(voting ~ civicduty+hawthorne+self+neighbors, data = gerber)
 summary(glm_grber)
-predict_grber <- predict(glm_grber,newdata = gerber)
+
+predict_grber <- predict(glm_grber)
+
 table(gerber$voting,predict_grber > 0.3)
+
+table(gerber$voting,predict_grber > 0.3)
+
+CARTmodel = rpart(voting ~ civicduty + hawthorne + self + neighbors, data=gerber)
+prp(CARTmodel)
